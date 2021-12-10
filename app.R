ba55320a6ce16ea6cb6d10457c0b1625e8f8b063
heidilunde <heididueholm@hotmail.com>
2021-12-10 11:02:43 +0100

    Applied tabset panel for table and plot(Used rnorm(100) for visualizing). The code for the datatable includes the "editable" suggestion for editing the description column, but is dosn't apply anything to the app.

--- app.R
+++ app.R
@@ -10,41 +10,61 @@
 library(shiny)
 library(tidyselect)
 
+
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("SustainScapes data"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with a slider input for loading a file
     sidebarLayout(
         sidebarPanel(
             fileInput("file", label = h3("Input data")),
         ),
 
-        # Show a plot of the generated distribution
+        # Show a table and a plot of the data
         mainPanel(
-           dataTableOutput("TableData")
+          tabsetPanel(
+            tabPanel("Table",DT::DTOutput("TableData")),
+            tabPanel("Plot",plotOutput("plot"))
+          )
+           
         )
         
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to show datatable and plot
 server <- function(input, output) {
-  output$TableData <- renderDataTable({
+  
+  #making the datatable
+  output$TableData <- DT::renderDT({
       file <- input$file
+      
+      #when no file is selected there will be no error
       validate(
-        need(input$file != "", " ")
+        need(input$file != "", " ") 
       )
-      ext <- tools::file_ext(file$datapath)
+      
       Vars <- colnames(read.csv(file$datapath))
+      
       DF <- data.frame(Variables = Vars, 
                        Description = NA, 
                        Units = NA, 
                        File = file$name,
                        path = file$datapath)
-      DF
+      
+      #making Description column editable
+      editable = list(target = 'column' , disable = list( columns =1,3,4,5) )
+      DF})
+  
+  #making the plot
+  output$plot <- renderPlot({
+    plot(rnorm(100),main="rnorm(100)")
+      
   })
 }
 
